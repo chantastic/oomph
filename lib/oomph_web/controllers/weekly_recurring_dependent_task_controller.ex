7b0e2e29dd37e169a47c0bc6c5d6db783c90e8c9
Michael Chan <mijoch@gmail.com>
2023-12-23 15:51:06 -0800

    get associated User record in show page

--- lib/oomph_web/controllers/weekly_recurring_dependent_task_controller.ex
+++ lib/oomph_web/controllers/weekly_recurring_dependent_task_controller.ex
@@ -3,6 +3,8 @@ defmodule OomphWeb.WeeklyRecurringDependentTaskController do
 
   alias Oomph.Tasks
   alias Oomph.Tasks.WeeklyRecurringDependentTask
+  alias Oomph.Accounts.User
+  alias Oomph.Repo
 
   def index(conn, _params) do
     weekly_recurring_dependent_tasks = Tasks.list_weekly_recurring_dependent_tasks()
@@ -28,32 +30,52 @@ defmodule OomphWeb.WeeklyRecurringDependentTaskController do
 
   def show(conn, %{"id" => id}) do
     weekly_recurring_dependent_task = Tasks.get_weekly_recurring_dependent_task!(id)
-    render(conn, :show, weekly_recurring_dependent_task: weekly_recurring_dependent_task)
+    author = Repo.get!(User, weekly_recurring_dependent_task.author_id)
+
+    render(conn, :show,
+      weekly_recurring_dependent_task: weekly_recurring_dependent_task,
+      author: author
+    )
   end
 
   def edit(conn, %{"id" => id}) do
     weekly_recurring_dependent_task = Tasks.get_weekly_recurring_dependent_task!(id)
     changeset = Tasks.change_weekly_recurring_dependent_task(weekly_recurring_dependent_task)
-    render(conn, :edit, weekly_recurring_dependent_task: weekly_recurring_dependent_task, changeset: changeset)
+
+    render(conn, :edit,
+      weekly_recurring_dependent_task: weekly_recurring_dependent_task,
+      changeset: changeset
+    )
   end
 
-  def update(conn, %{"id" => id, "weekly_recurring_dependent_task" => weekly_recurring_dependent_task_params}) do
+  def update(conn, %{
+        "id" => id,
+        "weekly_recurring_dependent_task" => weekly_recurring_dependent_task_params
+      }) do
     weekly_recurring_dependent_task = Tasks.get_weekly_recurring_dependent_task!(id)
 
-    case Tasks.update_weekly_recurring_dependent_task(weekly_recurring_dependent_task, weekly_recurring_dependent_task_params) do
+    case Tasks.update_weekly_recurring_dependent_task(
+           weekly_recurring_dependent_task,
+           weekly_recurring_dependent_task_params
+         ) do
       {:ok, weekly_recurring_dependent_task} ->
         conn
         |> put_flash(:info, "Weekly recurring dependent task updated successfully.")
         |> redirect(to: ~p"/weekly_recurring_dependent_tasks/#{weekly_recurring_dependent_task}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, weekly_recurring_dependent_task: weekly_recurring_dependent_task, changeset: changeset)
+        render(conn, :edit,
+          weekly_recurring_dependent_task: weekly_recurring_dependent_task,
+          changeset: changeset
+        )
     end
   end
 
   def delete(conn, %{"id" => id}) do
     weekly_recurring_dependent_task = Tasks.get_weekly_recurring_dependent_task!(id)
-    {:ok, _weekly_recurring_dependent_task} = Tasks.delete_weekly_recurring_dependent_task(weekly_recurring_dependent_task)
+
+    {:ok, _weekly_recurring_dependent_task} =
+      Tasks.delete_weekly_recurring_dependent_task(weekly_recurring_dependent_task)
 
     conn
     |> put_flash(:info, "Weekly recurring dependent task deleted successfully.")
