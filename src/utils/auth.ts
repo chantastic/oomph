2fc865482271e08041d7c56471688555d8a37b52
Michael Chan <mijoch@gmail.com>
2025-07-25 17:21:49 -0700

    lay waste

--- src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "@cvx/_generated/api";
-import { useQuery } from "@tanstack/react-query";
-import { convexQuery } from "@convex-dev/react-query";
-
-export function getCurrentUser() {
-  const { data: user } = useQuery(convexQuery(api.app.getCurrentUser, {}));
-
-  return user;
-}
