cfb877b29b95282cf52a584a740aa731620e82cd
Michael Chan <mijoch@gmail.com>
2025-09-30 10:30:50 -0700

    fix cron. run at 0900 UTC

---
+++ convex/crons.ts
@@ -0,0 +1,15 @@
+import { cronJobs } from "convex/server";
+import { api } from "./_generated/api";
+
+const crons = cronJobs();
+
+// Schedule materialization to run daily at 9:00 AM UTC
+// This corresponds to 1:00 AM PT (PST) or 2:00 AM PT (PDT)
+// Consistent UTC time regardless of daylight saving time
+crons.cron(
+  "task materialization",
+  "0 9 * * *",
+  api.assigneeAssignment.materializeForAllAssignees
+);
+
+export default crons;
