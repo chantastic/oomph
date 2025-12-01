f9dd4072d824f25282715cce9eefc2f960c9d203
Michael Chan <mijoch@gmail.com>
2025-12-01 09:44:11 -0800

    follow library instructions, with error
    https://github.com/get-convex/workos-authkit?tab=readme-ov-file

---
+++ convex/auth.ts
@@ -0,0 +1,50 @@
+// convex/auth.ts
+import { AuthKit, type AuthFunctions } from "@convex-dev/workos-authkit";
+import { components, internal } from "./_generated/api";
+import type { DataModel } from "./_generated/dataModel";
+
+const authFunctions: AuthFunctions = internal.auth;
+
+const authKit = new AuthKit<DataModel>(components.workOSAuthKit, {
+  authFunctions,
+});
+
+export const { authKitEvent } = authKit.events({
+  "user.created": async (ctx, event) => {
+    await ctx.db.insert("user", {
+      authId: event.data.id,
+      email: event.data.email,
+      name: `${event.data.firstName} ${event.data.lastName}`,
+    });
+  },
+  "user.updated": async (ctx, event) => {
+    const user = await ctx.db
+      .query("user")
+      .withIndex("authId", (q) => q.eq("authId", event.data.id))
+      .unique();
+    if (!user) {
+      console.warn(`User not found: ${event.data.id}`);
+      return;
+    }
+    await ctx.db.patch(user._id, {
+      email: event.data.email,
+      name: `${event.data.firstName} ${event.data.lastName}`,
+    });
+  },
+  "user.deleted": async (ctx, event) => {
+    const user = await ctx.db
+      .query("user")
+      .withIndex("authId", (q) => q.eq("authId", event.data.id))
+      .unique();
+    if (!user) {
+      console.warn(`User not found: ${event.data.id}`);
+      return;
+    }
+    await ctx.db.delete(user._id);
+  },
+
+  // Handle any event type
+  "session.created": async (ctx, event) => {
+    console.log("onCreateSession", event);
+  },
+});
